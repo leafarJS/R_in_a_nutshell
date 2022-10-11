40c20723da4a14c6653752ae9348235be28a9d35
unknown <riskeconomics@gmail.com>
2022-10-10 22:38:19 -0400

    commit matrixs

---
+++ 03_matrix.R
@@ -0,0 +1,9 @@
+# Matrices 
+m <- matrix(data = 1:12,
+            nrow = 4,
+            ncol = 3,
+            dimnames = list(c("r1","r2","r3","r4"),
+                            c("c1","c2","c3")))
+
+m
+
