805856cb2327f4e8090dab0e993c43ce9111afa7
unknown <riskeconomics@gmail.com>
2022-10-10 22:57:46 -0400

    commit factors

---
+++ .Rproj.user/EB28C2AB/sources/session-EB0D8994/66911B3E-contents
@@ -0,0 +1,38 @@
+#VECTORS
+# a vector of five numbers
+v <- c(.295,.300,.250,.287,.215)
+v
+
+# creating a vector from four numbers and a char
+v<- c(.295,.300,.250,.287, "jorge")
+v
+
+# creating a vector form four numbers a list of
+# three more
+v <- c(.295,.300,.250,.287, list(.102, .200, .300), recursive = TRUE)
+v
+typeof(v)
+
+v <- c(.295,.300,.250,.287, list(1,2,3))
+typeof(v)
+class(v)
+v
+
+# This operator creates a sequence of values from the first operand to the second operand
+1:10
+
+# a more flexible function 
+seq(from = 5, to = 25, by = 5)
+
+# you can explicitly manipulate the length of a vector through the length attribute
+w <- 1:10
+w
+length(w)
+
+#change
+length(w) <- 7
+w
+
+#Note that when you expand the length of a vector, uninitialized values are given the NA value
+length(w) <- 15
+w
---
+++ 05_factors.R
@@ -0,0 +1,41 @@
+#FACTORS
+eye_colors <- c("brown", "blue", "blue", "blue", "green", "brown", "brown", "brown")
+eye_colors
+
+
+eye_colors <-factor(c("brown", "blue", "blue", "blue", "green", "brown", "brown", "brown"))
+#The levels function shows all the levels from a factor:
+eye_colors
+levels(eye_colors)
+
+
+#Para sortear estos problemas, puede usar un factor ordenado para representar 
+#la respuesta de esta encuesta. Aquí hay un ejemplo:
+
+survey_result <-factor(c("Disagree","Neutral","Strongly Disagree",
+                       "Neutral","Agree","Strongly Agree",
+                       "Disagree","Strongly Agree","Neutral",
+                       "Strongly Disagree","Neutral","Agree"),
+                       levels = c("Strongly Disagree", "Disagree",
+                                  "Neutral", "Agree", "Strongly Agree"),
+                       ordered = TRUE)
+survey_result
+
+
+
+#use the eye colors vector we used above
+eye_colors
+class(eye_colors)
+
+#now create a vector by removing the class
+eye_colors.integer.vector <- unclass(eye_colors)
+eye_colors.integer.vector
+
+attr(,"levels")
+class(eye_colors.integer.vector)
+
+#t’s possible to change this back to a factor by setting the class attribute:
+class(eye_colors.integer.vector) <- "factor"
+eye_colors.integer.vector
+
+class(eye_colors.integer.vector)
